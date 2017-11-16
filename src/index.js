ab872fdcd6efbb58305287b22c9de3c7da2cda9d
Zacharias Enochsson <zache@axis.com>
2017-11-16 08:30:20 +0100

    change signature to (S, A, D1, D2)

--- src/index.js
+++ src/index.js
@@ -1,59 +1,56 @@
 function mapObj(orig, fn, onto) {
     return Object.keys((orig || {})).reduce((o, name) => {
-      o[name] = fn(orig[name], name, o)
-      return o
+        o[name] = fn(orig[name], name, o)
+        return o
     }, (onto || {}))
-  } 
-  
-  function bindFnCollection(binder, collection, ...more) {
+} 
+
+function bindFnCollection(binder, collection, ...more) {
     return mapObj(collection, (toBind, name, bound) => {
-      if (typeof toBind === 'function') return binder(toBind, bound, ...more)
-      return bindFnCollection(binder, toBind, ...more.map(x => x[name]))
+        if (typeof toBind === 'function') return binder(toBind, bound, ...more)
+        return bindFnCollection(binder, toBind, ...more.map(x => x[name]))
     })
-  }
-  
-  function collectSubs(opts, prop) {
+}
+
+function collectSubs(opts, prop) {
     return mapObj(opts.sub, sub => Object.assign({}, collectSubs(sub, prop)), opts[prop])
-  }
-  
-  function onceNextTick (fn) {
+}
+
+function onceNextTick (fn) {
     var pending;
     return _ => {
-      if (pending) return
-      pending = !pending
-      setTimeout(_ => {
+        if (pending) return
+        pending = !pending
+        setTimeout(_ => {
         pending = !pending
         fn()
-      }, 0)
+        }, 0)
     }
-  }
-  
-  function curriedCaller (fn, bound, ...more) {
-    return  function (...args) {
-      var ret = fn(...more, bound)
-      if (typeof ret === 'function') ret = ret(...args)
-      return ret
-    }
-  }
-  
-  function actionCaller (update) {
-    return function (action, actions, state) {
-      var caller = curriedCaller(action, actions, state)
-      return  function (...args) {
-        var ret = caller(...args)
-        if (!ret) return
-        Object.assign(state, ret)
-        update()
-      }
-    }
-  }
-  
-  
-  export default function (opts, notify) {
+}
+
+export default function (opts, notify) {
+    
     const update = onceNextTick(notify || (_ => {})) 
     const state = collectSubs(opts, 'state')
-    const actions = bindFnCollection(actionCaller(update), collectSubs(opts, 'actions'), state)
-    const views = bindFnCollection(curriedCaller, collectSubs(opts, 'views'), state, actions)
+
+    const actions = bindFnCollection(
+        (fn, A, S) =>  (...args) => {
+            var ret = fn(A, A, ...args)
+            if (!ret) return
+            Object.assign(state, ret)
+            update()
+        },
+        collectSubs(opts, 'actions'),
+        state
+    )
+
+    const views = bindFnCollection(
+        (fn, V, S, A) => (...args) => fn(S, A, V, ...args),
+        collectSubs(opts, 'views'),
+        state,
+        actions
+    )
+
     update()
     return {actions, views}
-  }
+}
