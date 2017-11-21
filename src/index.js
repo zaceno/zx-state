b7db6d1131e4adae1e865a7153aab938957cd6d9
Zacharias Enochsson <zache@axis.com>
2017-11-21 20:36:00 +0100

    change api

--- src/index.js
+++ src/index.js
@@ -1,54 +1,32 @@
-function mapObj(orig, fn, onto) {
-    return Object.keys((orig || {})).reduce((o, name) => {
-        o[name] = fn(orig[name], name, o)
-        return o
-    }, (onto || {}))
-} 
-
-function bindFnCollection(binder, collection, ...more) {
-    return mapObj(collection, (toBind, name, bound) => {
-        if (typeof toBind === 'function') return binder(toBind, bound, ...more)
-        return bindFnCollection(binder, toBind, ...more.map(x => x[name]))
-    })
+const mapFunctionCollection = mapping => coll => {
+    if (typeof coll == 'function') return mapping(coll)
+    return Object.keys(coll).reduce((mapped, name) => {
+        mapped[name] = mapping(coll[name])
+        return mapped
+    }, {})
+}
+const store = onupdate => {
+    var state = {}
+    const set = diff => {
+        state = Object.assign({}, state, diff)
+        onupdate()
+    }
+    return {
+        init: set,
+        getter: mapFunctionCollection(f => (...args) => f(state, ...args)),
+        setter: mapFunctionCollection(f => (...args) => set(f(state, ...args))),
+        component: mapFunctionCollection(f => f(store(onupdate))),
+    }    
 }
 
-function collectSubs(opts, prop) {
-    return mapObj(opts.sub, sub => Object.assign({}, collectSubs(sub, prop)), opts[prop])
-}
-
-export default function (opts, notify) {
-    var pending;
-
-    const update = _ => {
+export default (main, onupdate) => {
+    var pending = false
+    return main(store(_ => {
         if (pending) return
         pending = !pending
         setTimeout(_ => {
-           pending = !pending
-           notify && notify()
-        }, 0)
-    }
-    
-    const state = collectSubs(opts, 'state')
-
-    const actions = bindFnCollection(
-        (fn, actions, state) =>  (...args) => {
-            var data = fn(state, actions, ...args)
-            if (!data) return
-            Object.assign(state, data)
-            update()
-        },
-        collectSubs(opts, 'actions'),
-        state
-    )
-
-    const views = bindFnCollection(
-        (fn, V, S, A) => (...args) => fn(S, A, V, ...args),
-        collectSubs(opts, 'views'),
-        state,
-        actions
-    )
-
-    update()
-
-    return {actions, views}
+            pending = !pending
+            onupdate()
+        })
+    }))
 }
